057f374d240b1629e5c9942823acfcee2be864fd
Takafumi Arakaki <aka.tkf@gmail.com>
2019-09-22 21:28:15 -0700

    Use custom solution type

--- src/solvers.jl
+++ src/solvers.jl
@@ -1,5 +1,5 @@
 """
-    solve(sso::SteadyStateObjective, [method, options, x0])
+    solve(sso::SteadyStateObjective, [method, options, x0]) :: SteadyStateFitResult
 
 Optimize a [`SteadyStateObjective`](@ref) `sso` using Optim.  The rest
 of the positional and keyword arguments are passed to
@@ -12,13 +12,58 @@ function solve(
     x0 = get(sso.p, sso.parameterlens);
     kwargs...,
 )
-    return Optim.optimize(
+    result = Optim.optimize(
         Optim.only_fg!(sso),
         x0,
         method,
         setupoptions(sso, options);
         kwargs...,
     )
+    return SteadyStateFitResult(result, sso)
+end
+
+"""
+    SteadyStateFitResult
+
+# Fields
+- `result :: Optim.OptimizationResults`
+- `objective :: SteadyStateObjective`
+
+# Methods
+- `minimum(fit::SteadyStateFitResult)`: Equivalent to `minimum(fit.result)`.
+- `Optim.minimizer(fit::SteadyStateFitResult)`: Return a copy of
+  `fit.objective.p` with parameters replaced by `Optim.minimizer(fit.result)`.
+"""
+struct SteadyStateFitResult
+    result  # Optim.MultivariateOptimizationResults
+    objective  # SteadyStateObjective
+end
+
+function Base.show(io::IO, ::MIME"text/plain", fit::SteadyStateFitResult)
+    sso = fit.objective
+    println(io, """
+    SteadyStateFitResult
+     * Loss:
+        $(summary(sso.loss))
+
+     * Model:
+        $(summary(sso.p))
+
+     * Parameters:
+        $(sso.parameterlens)
+
+     * Conditions ($(length(sso.conditions))):
+        $(sso.conditionsetter)
+    """)
+    show(io, MIME("text/plain"), fit.result)
+end
+
+Base.minimum(fit::SteadyStateFitResult) = minimum(fit.result)
+
+function Optim.minimizer(fit::SteadyStateFitResult)
+    p = fit.objective.p
+    parameterlens = fit.objective.parameterlens
+    return set(p, parameterlens, Optim.minimizer(fit.result))
 end
 
 # A hacky solution: To use the steady state of the last iteration,
